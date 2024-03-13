7284facc5c878c45a0f81b7a7e610697d84c301d
Huzaifaabdulrab <149306467+Huzaifaabdulrab@users.noreply.github.com>
2024-03-14 00:03:59 +0500

    Delete app.ts

--- app.ts
+++
@@ -1,7 +0,0 @@
-// 3. Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
-
-let firstname : string ="huzaifa abdulrab";
-
- console.log (firstname.toLocaleUpperCase())
- console.log (firstname.toLocaleLowerCase())
- console.log (firstname.charAt(0).toLocaleUpperCase() + firstname.slice(1));
