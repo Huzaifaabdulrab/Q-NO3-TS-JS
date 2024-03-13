1c31762e498af1284c0330c4f9de123128fb95be
Huzaifaabdulrab <149306467+Huzaifaabdulrab@users.noreply.github.com>
2024-03-14 00:01:31 +0500

    Add files via upload

---
+++ Qno3.js
@@ -0,0 +1,7 @@
+"use strict";
+// 3. Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+Object.defineProperty(exports, "__esModule", { value: true });
+let first_name = "huzaifa";
+console.log(first_name.toLowerCase());
+console.log(first_name.toUpperCase());
+console.log(first_name.charAt(0).toUpperCase() + first_name.slice(1));
---
+++ Qno3.ts
@@ -0,0 +1,7 @@
+// 3. Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+let first_name: string = "huzaifa";
+
+console.log(first_name.toLowerCase());
+console.log(first_name.toUpperCase());
+console.log(first_name.charAt(0).toUpperCase()+ first_name.slice(1));
